6b62f58c226dd59646162d497a25d36f19a5e37e
Kevin Krato <kratok08@gmail.com>
2019-12-24 15:18:38 -0700

    Setup initial api interactions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Dashboard from './components/Dashboard/Dashboard';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/dashboard" component={Dashboard} />
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
