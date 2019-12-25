6795f114ea8d524b5449c6164fcbe95d30ec84e7
Kevin Krato <kratok08@gmail.com>
2019-12-25 14:53:21 -0700

    Combine Login/Register components into one AuthForms component

---
+++ src/components/_auth/AuthForms.js
@@ -0,0 +1,143 @@
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
+
+export default class AuthForms extends Component {
+	state = {
+		email: '',
+		password: '',
+		password_confirmation: '',
+		error: ''
+	};
+
+	handleSuccessfulAuth = (data) => {
+		this.props.handleLogin(data);
+		this.props.history.push('/dashboard');
+	};
+
+	handleChange = (e) => {
+		const { name, value } = e.target;
+		this.setState({ [name]: value });
+	};
+
+	handleRegistration = (e) => {
+		e.preventDefault();
+		const { email, password, password_confirmation } = this.state;
+		axios
+			.post(
+				'http://localhost:3001/registrations',
+				{
+					user: {
+						email: email,
+						password: password,
+						password_confirmation: password_confirmation
+					}
+				},
+				{ withCredentials: true }
+			)
+			.then((response) => {
+				if (response.data.status === 200) {
+					this.handleSuccessfulAuth(response.data);
+				}
+				console.log(response);
+			});
+		// .catch((error) => {
+		// 	console.log('registration error', error);
+		// });
+	};
+
+	handleLogin = (e) => {
+		e.preventDefault();
+		axios
+			.post(
+				'http://localhost:3001/sessions',
+				{
+					user: { email: this.state.email, password: this.state.password }
+				},
+				{ withCredentials: true }
+			)
+			.then((response) => {
+				console.log(response);
+				if (response.data.logged_in) {
+					this.handleSuccessfulAuth(response.data);
+				}
+			})
+			.catch((error) => {
+				console.log('login error', error);
+			});
+	};
+
+	render() {
+		const registrationForm = (
+			<section>
+				<h4>{this.props.loggedInStatus}</h4>
+				<h2>Registration Form</h2>
+				<form onSubmit={this.handleRegistration}>
+					<input
+						type="email"
+						name="email"
+						placeholder="Email"
+						value={this.state.email}
+						onChange={this.handleChange}
+						required
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"
+						value={this.state.password}
+						onChange={this.handleChange}
+						required
+					/>
+					<input
+						type="password"
+						name="password_confirmation"
+						placeholder="Password Confirmation"
+						value={this.state.password_confirmation}
+						onChange={this.handleChange}
+						required
+					/>
+
+					<button type="submit">Register</button>
+				</form>
+			</section>
+		);
+
+		const loginForm = (
+			<section>
+				<h4>{this.props.loggedInStatus}</h4>
+				<h2>Login Form</h2>
+				<form onSubmit={this.handleLogin}>
+					<input
+						type="email"
+						name="email"
+						placeholder="Email"
+						value={this.state.email}
+						onChange={this.handleChange}
+						required
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"
+						value={this.state.password}
+						onChange={this.handleChange}
+						required
+					/>
+					<button type="submit">Login</button>
+				</form>
+			</section>
+		);
+
+		const errorReport = (
+			<div>
+				<h2>{this.state.error}</h2>
+			</div>
+		);
+		return (
+			<Fragment>
+				{this.state.error && errorReport}
+				{this.props.history.location.pathname === '/login' ? loginForm : registrationForm}
+			</Fragment>
+		);
+	}
+}
