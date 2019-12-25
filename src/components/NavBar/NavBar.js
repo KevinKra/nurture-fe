3e148638fc09bf341f0335e31643833b5950de4d
Kevin Krato <kratok08@gmail.com>
2019-12-25 16:33:08 -0700

    Conditionally render navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.scss';
 
 export default function NavBar(props) {
-	return (
-		<nav className="top-nav">
-			<Link to="/home">Home</Link>
-			<Link to="/register">Register</Link>
-			<Link to="/login">Login</Link>
-			<button onClick={props.handleLogout}>Logout</button>
-		</nav>
-	);
+	const navbarFormat =
+		props.loggedInStatus === 'LOGGED_IN' ? (
+			<Fragment>
+				<Link to="/">Home</Link>
+				<button onClick={props.handleLogout}>Logout</button>
+			</Fragment>
+		) : (
+			<Fragment>
+				<Link to="/">Home</Link>
+				<Link to="/register">Register</Link>
+				<Link to="/login">Login</Link>
+			</Fragment>
+		);
+
+	return <nav className="top-nav">{navbarFormat}</nav>;
 }
