2d0817d3a1f30a99eafc7930343073dadbc045f1
Kevin Krato <kratok08@gmail.com>
2019-12-25 17:06:26 -0700

    Write snapshot conditional tests for NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,8 +5,6 @@ import './NavBar.scss';
 
 export default function NavBar(props) {
 	const handleLogout = () => {
-		// needs to have access to router for redirect, but also needs to update app state to logout.
-		// if only i was using redux :c
 		axios.delete('http://localhost:3001/api/v1/logout', { withCredentials: true });
 		props.logoutUser();
 		props.history.push('/');
